247d69104236046b95855012d39c5ca3df36729f
Michael Madden <mikemadden42@icloud.com>
2022-05-20 06:32:43 -0500

    Initial ch01.

---
+++ ch02/Sources/ch02/main.swift
@@ -0,0 +1,6 @@
+var lights: Int = 4
+var population: Int = 5422
+let townName: String = "Somewhere"
+let elevation: Int = 1234
+let townDescription = "\(townName) - population \(population) - \(lights) lights - elevation \(elevation)"
+print(townDescription)
