b9d69f086900cdaf8e54d0f75059ab2a6a365feb
Michael Madden <mikemadden42@icloud.com>
2022-12-10 15:44:21 -0600

    Run swiftformat.

--- ch02/Package.swift
+++ ch02/Package.swift
@@ -14,9 +14,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "ch02",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "ch02Tests",
-            dependencies: ["ch02"]),
+            dependencies: ["ch02"]
+        ),
     ]
 )
